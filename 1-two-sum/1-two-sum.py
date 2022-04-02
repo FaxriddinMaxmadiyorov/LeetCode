d14a679e4f509d1c6d7d6ceb3f915bd1078e47ce
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-04-02 10:22:41 +0500

    Time: 70 ms (82.53%), Space: 15.6 MB (9.76%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+#         3213213
         dict = {}
         for i in range(len(nums)):
             dict[target - nums[i]] = i
