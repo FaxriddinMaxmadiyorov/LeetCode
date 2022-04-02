756096a699c63502ac71b9e5cdb8bc24d7f0dce9
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-04-02 10:20:39 +0500

    Time: 104 ms (52.97%), Space: 15.5 MB (9.76%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        indices = []
+        dict = {}
+        first, second = 0, 0
+        for i in range(len(nums)):
+            dict[target - nums[i]] = i
+        for i in range(len(nums)):
+            if nums[i] in dict and i != dict[nums[i]]:
+                return i, dict[nums[i]]                
