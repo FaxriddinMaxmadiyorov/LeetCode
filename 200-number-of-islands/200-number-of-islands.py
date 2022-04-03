870ca6cb7a91200cc91178b77d26e9bde2cb37d1
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-04-03 14:26:19 +0500

    Time: 375 ms (66.51%), Space: 16.2 MB (97.94%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,19 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def dfs(grid, i, j):
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
-                return
-            grid[i][j] = '0'
-            dfs(grid, i - 1, j)
-            dfs(grid, i + 1, j)
-            dfs(grid, i, j + 1)
-            dfs(grid, i, j - 1)
+        def yesno(i, j, m, n):
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return False
+            return True
         
+        m = len(grid)
+        n = len(grid[0])
         cnt = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        x_move = [1, -1, 0, 0]
+        y_move = [0, 0, 1, -1]
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == '1':
-                    dfs(grid, i, j)
                     cnt += 1
+                    grid[i][j] = '0'
+                    x = collections.deque()
+                    y = collections.deque()
+                    x.append(i)
+                    y.append(j)
+                    while len(x) != 0:
+                        fx, fy = x[len(x) - 1], y[len(y) - 1]
+                        x.pop()
+                        y.pop()
+                        for u in range(4):
+                            if yesno(fx + x_move[u], fy + y_move[u], m, n) and grid[fx + x_move[u]][fy + y_move[u]] == '1':
+                                x.appendleft(fx + x_move[u])
+                                y.appendleft(fy + y_move[u])
+                                grid[fx + x_move[u]][fy + y_move[u]] = '0'
         return cnt
