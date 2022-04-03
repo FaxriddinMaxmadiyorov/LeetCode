cc968fff2aec4dc7f97fe9b93a1172efc714313c
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-04-03 21:16:07 +0500

    Time: 43 ms (53.81%), Space: 14 MB (20.57%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -5,6 +5,5 @@ class Solution:
             temp = []
             for i in range(0, k + 1):
                 temp.append(math.factorial(k) // (math.factorial(i) * math.factorial(k - i)))
-                # print(k, i)
             res.append(temp)
         return res
