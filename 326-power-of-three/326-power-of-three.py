2581d6d91088bae66c3c3499029ee739eed6a143
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-04-04 11:05:47 +0500

    Time: 90 ms (77.75%), Space: 13.9 MB (20.49%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,22 +1,3 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        def find_ternary(num):  #2
-            quotient = num/3    #3
-            remainder = num%3
-            if quotient == 0:   #4
-                return ""
-            else:
-                return find_ternary(int(quotient)) + str(int(remainder)) 
-        cnt, cn = 0, 0
-        nn = find_ternary(n)
-        print(nn)
-        if n > 0:
-            for i in nn:
-                if i == '1':
-                    cnt += 1
-                elif i == '0':
-                    cn += 1
-            # print(cnt, cn)
-            return cnt == 1 and cnt + cn == len(nn)
-        else:
-            return False
+        return n > 0 and 3 ** 100 % n == 0
