99432ddb2bfc92c85df8b1aa242c9ebce271042a
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-04-19 10:01:15 +0500

    Time: 53 ms (39.88%), Space: 14 MB (52.48%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,10 +1,22 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str: 
-        for i in range(len(strs[0])):
-            char = strs[0][i]
-            for j in range(1, len(strs)):
-                if len(strs[j]) > i and char == strs[j][i]:
-                    continue
-                else:
-                    return strs[0][:i]
-        return strs[0]
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        minLen = 10000000000000000000000000000
+        for i in strs:
+            minLen = min(minLen, len(i))
+            
+        low, high = 1, minLen
+        
+        while low <= high:
+            mid = (low + high) // 2
+            if self.isCommonPref(strs, mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        return strs[0][:(low + high) // 2]
+    
+    def isCommonPref(self, strs, size):
+        s0 = strs[0][:size]
+        for i in range(1, len(strs)):
+            if strs[i][:size] != s0:
+                return False
+        return True
