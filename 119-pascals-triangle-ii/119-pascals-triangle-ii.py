3773b9ff78099c1c2a9f420c01fec7cf35a01afa
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-05-02 19:19:47 +0500

    Time: 31 ms (88.17%), Space: 14 MB (18.18%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -1,11 +1,5 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        a = [[0 for i in range(rowIndex + 1)] for i in range(rowIndex + 1)]
-        for i in range(rowIndex + 1):
-            for j in range(i + 1):
-                a[i][j] = 1
-                
-        for i in range(2, rowIndex + 1):
-            for j in range(i + 1):
-                a[i][j] = a[i-1][j-1] + a[i-1][j]
-        return a[i][:rowIndex + 1]
+        return [self.combinations(rowIndex, i) for i in range(rowIndex + 1)]
+    def combinations(self, n, m):
+        return math.factorial(n) // (math.factorial(m) * math.factorial(n - m))
