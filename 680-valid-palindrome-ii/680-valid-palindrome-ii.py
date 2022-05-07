6bdf34c26c0f323dcdf3c7621f23419d2763a1ab
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-05-07 16:15:55 +0500

    Time: 296 ms (17.58%), Space: 14.7 MB (12.56%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,18 +1,29 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        if self.check(s):
+        def check(i, j):
+            s1 = s[i:j]
+            s2 = s[i+1:j+1]
+            print(s1, s2)
+            d = True
+            e = True
+            for k in range(len(s1) // 2):
+                if s1[k] != s1[len(s1) - k - 1]:
+                    d = False
+            
+            for k in range(len(s2) // 2):
+                if s2[k] != s2[len(s2) - k - 1]:
+                    e = False
+            return d == True or e == True
+        
+        if s == s[::-1]:
             return True
+        print("hjh")
         i, j = 0, len(s) - 1
         while i < j:
             if s[i] != s[j]:
-                if self.check(s[i:j]) or self.check(s[i+1:j+1]):
+                print(i, j)
+                if check(i, j):
                     return True
                 return False
             i += 1
             j -= 1
-        
-    def check(self, s):
-        for i in range(len(s) // 2):
-            if s[i] != s[len(s) - i - 1]:
-                return False
-        return True
