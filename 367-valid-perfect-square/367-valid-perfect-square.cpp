889511190307e946b4b9c392f66bb8ab92bca7a8
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-05-14 13:18:20 +0500

    Time: 0 ms (100.00%), Space: 6 MB (17.86%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        vector<int> squares;
-        for (int i=1; i<=46340; i++)
-            squares.push_back(i*i);
-        for (auto i:squares){
-            if (i==num)
-                return true;
+        int i = 1;
+        while (num > 0){
+            num = num - i;
+            i += 2;
         }
-        return false;
+        return num == 0;
     }
 };
