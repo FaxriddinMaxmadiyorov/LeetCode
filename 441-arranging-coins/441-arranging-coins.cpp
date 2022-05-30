49720eeeee99f1d731646274b480a3e6ad9e4768
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-05-30 11:47:39 +0500

    Time: 6 ms (51.63%), Space: 6 MB (27.42%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int arrangeCoins(int n) {
-        int t = 1, k = 0;
-        while (n > 0)
+    int arrangeCoins(long long n) {
+        long long left = 1, right = 123456789;
+        while (left <= right)
         {
-            if (n - t >= 0)
-                n -= t, k++, t++;
-            else
-                break;
+            long long middle = (left + right) / 2;
+            long long sum = middle * (middle + 1) / 2;
+            if (sum > n)
+                right = middle - 1;
+            else if (sum < n)
+                left = middle + 1;
+            else 
+                return middle;
         }
-        return k;
+        return right;
     }
 };
