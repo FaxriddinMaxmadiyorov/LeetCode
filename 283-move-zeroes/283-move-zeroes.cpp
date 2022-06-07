be67eb6e45cfded0c7a4df633ff0f3fccac8d565
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-06-08 01:23:25 +0500

    Time: 33 ms (51.19%), Space: 19.3 MB (58.59%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int start = 0;
-        for (int i=0; i<nums.size(); i++)
-        {
-            if (nums[i] == 0)
-            {
-                int start = i;
-                while (nums[start] == 0)
-                {
-                    start++;
-                    if (start >= nums.size())
-                        return;
-                }
-                swap(nums[i], nums[start]);
-            }
+        for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+        if (nums[cur] != 0) {
+            swap(nums[lastNonZeroFoundAt++], nums[cur]);
         }
+    }
     }
 };
