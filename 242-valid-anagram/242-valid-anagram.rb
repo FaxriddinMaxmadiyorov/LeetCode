5cdb2707c3d892b606e0a1d2bb3c82841caec8db
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-06-15 20:49:33 +0500

    Time: 143 ms (85.32%), Space: 211.6 MB (82.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.rb
+++ 242-valid-anagram/242-valid-anagram.rb
@@ -3,17 +3,20 @@
 # # @return {Boolean}
 def is_anagram(s, t)
   ss = Hash.new(0)
-  tt = Hash.new(0)
    
   s.each_char do |i|
         ss[i] += 1    
   end
    
   t.each_char do |i|
-        tt[i] += 1    
+        ss[i] -= 1    
   end
-  ss == tt
-  # return true
+  ss.each do |key, value|
+     if value != 0
+        return false 
+     end
+  end
+return true
 end
 is_anagram("jh", "klsdkf")
 # s = "huyuy"
