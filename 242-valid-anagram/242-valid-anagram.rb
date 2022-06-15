3d1337362e15b8fb6a6f4d41a25f711af82507ea
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-06-15 20:37:10 +0500

    Time: 137 ms (89.11%), Space: 211.8 MB (61.01%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.rb
+++ 242-valid-anagram/242-valid-anagram.rb
@@ -1,8 +1,32 @@
-# @param {String} s
-# @param {String} t
-# @return {Boolean}
+# # @param {String} s
+# # @param {String} t
+# # @return {Boolean}
 def is_anagram(s, t)
-   s = s.chars.sort.join('')
-   t = t.chars.sort.join('')
-   s == t
+  ss = Hash.new(0)
+  tt = Hash.new(0)
+   
+  s.each_char do |i|
+        ss[i] += 1    
+  end
+   
+  t.each_char do |i|
+        tt[i] += 1    
+  end
+   
+  ss.each do |key, value|
+    if ss[key] != tt[key]
+        return false  
+    end
+  end
+    tt.each do |key, value|
+    if ss[key] != tt[key]
+        return false  
+    end
+  end
+  return true
 end
+is_anagram("jh", "klsdkf")
+# s = "huyuy"
+# s.each_char do |char|
+#     puts char
+# end
