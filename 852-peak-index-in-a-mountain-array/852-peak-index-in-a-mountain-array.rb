309f6cb2dec5c9cb5d2f184bfe5f4de8492b1f34
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-06-25 12:30:51 +0500

    Time: 165 ms (5.97%), Space: 211.7 MB (13.43%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.rb
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.rb
@@ -2,7 +2,7 @@
 # @return {Integer}
 def peak_index_in_mountain_array(arr)
     l, r = 0, arr.length - 1
-    while l < r do
+    while l < r 
        m = (l + r) / 2
        if arr[m] < arr[m + 1]
            l = m + 1
