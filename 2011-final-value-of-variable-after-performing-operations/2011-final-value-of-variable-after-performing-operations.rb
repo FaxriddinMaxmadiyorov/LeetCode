f8a7beaae20810be4297293eead5713d02a7e487
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-07-16 08:12:57 +0500

    Time: 86 ms (95.65%), Space: 211.1 MB (10.87%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.rb
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.rb
@@ -3,7 +3,7 @@
 def final_value_after_operations(operations)
     x = 0
     for oper in operations
-       if oper[0] == '+' || oper[1] == '+'
+       if oper[1] == '+'
           x += 1
         else
            x -= 1
