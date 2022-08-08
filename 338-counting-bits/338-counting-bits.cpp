247dc4c4e5906e338e611550b9dae4abcb758f9c
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-08-08 22:26:04 +0500

    Time: 203 ms (5.11%), Space: 8.4 MB (33.04%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> a;
+        // 0 -> 0          0 
+        // 1 -> 1          1
+        // 2 -> 1          1
+        // 3 -> 11         2
+        // 4 -> 100        1
+        // 5 -> 101        2
+        // 6 -> 110        2
+        // 7 -> 111        3
+        // 8 -> 1000       1
+        // 9 -> 1001       2
+        // 10 -> 1010      2
+        // 11 -> 1011      3
+        // 12 -> 1100      2
+        // 13 -> 1101      3
+        // 14 -> 1110      3
+        // 15 -> 1111      4
+        // 16 -> 10000     1
+        a.push_back(0);
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 1)
+                a.push_back(a[i - 1] + 1);
+            else
+                a.push_back(a[i / 2]);
+        }
+        for (int i = 0; i <= n; i++)
+            cout<<i<<" -> "<<a[i]<<endl;
+        return a;
+    }
+};
