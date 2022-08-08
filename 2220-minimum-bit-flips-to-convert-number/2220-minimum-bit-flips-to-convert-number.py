ec247e4f9b3bfdae5dc567cd7bf9ab93b1c79c04
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-08-08 23:08:57 +0500

    Time: 111 ms (6.39%), Space: 13.8 MB (66.03%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        start = '{0:0160b}'.format(start)
+        goal = '{0:0160b}'.format(goal)
+        c = 0
+        print(start, goal)
+        for i in range(len(start)):
+            print(i, len(start))
+            if start[i] != goal[i]:
+                c += 1
+                
+        return c
