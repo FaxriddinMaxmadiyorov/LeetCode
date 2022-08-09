0d0b085923db0b8ce1386ec3a19bfe9022d0043f
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-08-09 11:53:59 +0500

    Time: 55 ms (32.69%), Space: 13.8 MB (97.45%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,24 +1,10 @@
-class Solution {
-public:
-    string DecimalToBinary(int n){
-        string res = "";
-        while (n > 0){
-            res += char(n % 2 + 48);
-            n = n / 2;
-        }
-        res += char(n % 2 + 48);
-        reverse(res.begin(), res.end());
-        return res;
-    }
-    
-    int minBitFlips(int start, int goal) {
-        int temp = start ^ goal, c = 0;
-        string BinaryXor = DecimalToBinary(temp);
-        for (int i = 0; i < BinaryXor.size(); i++)
-        {
-            if (BinaryXor[i] == '1')
-                c++;
-        }
-        return c;
-    }
-};
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        start = '{0:030b}'.format(start)
+        goal = '{0:030b}'.format(goal)
+        c = 0
+        for i in range(len(start)):
+            if start[i] != goal[i]:
+                c += 1
+                
+        return c
