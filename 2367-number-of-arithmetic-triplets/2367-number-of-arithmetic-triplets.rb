65412f37c0416dc12b97c9bfe1991decdeadc1d5
FaxriddinMaxmadiyorov <51068374+FaxriddinMaxmadiyorov@users.noreply.github.com>
2022-08-13 12:22:30 +0500

    Time: 159 ms (100.00%), Space: 211.2 MB (100.00%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.rb
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.rb
@@ -1,20 +1,12 @@
-class Solution {
-public:
-    int arithmeticTriplets(vector<int>& nums, int diff) {
-        int c = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j= i + 1; j < nums.size(); j++)
-            {
-                int diff1 = nums[j] - nums[i];
-                for (int k = j + 1; k < nums.size(); k++)
-                {
-                    int diff2 = nums[k] - nums[j];
-                    if (diff1 == diff2 && diff1 == diff)
-                        c++;
-                }
-            }
-        }
-        return c;
-    }
-};
+# @param {Integer[]} nums
+# @param {Integer} diff
+# @return {Integer}
+def arithmetic_triplets(nums, diff)
+    c = 0
+    for num in nums
+        if nums.include?(num + diff) and nums.include?(num + 2*diff)
+            c += 1
+        end
+    end
+    c
+end
